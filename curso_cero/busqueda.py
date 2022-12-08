2d90f84d0cf9ec54c4911618c6a0cba4a4fe7aab
WhiteArchangel <angel.ordonez.r@outlook.com>
2022-12-07 23:57:35 -0600

    Clase 3 Parte 3 - La Búsqueda.

--- curso_cero/busqueda.py
+++ curso_cero/busqueda.py
@@ -1,3 +1,3 @@
-mensaje = "Se utiliza la manipulación de cadena con Búsqueda para encontrar la posición de la subcadena deseada."
+mensaje = "Se utiliza la manipulación de cadena con Búsqueda para encontrar la posición de la subcadena requerida."
 subcadena = mensaje.find("Búsqueda")
 print(subcadena)
