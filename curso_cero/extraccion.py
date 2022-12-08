1567542ab81442e7e0be388d8c091c936589bf7d
WhiteArchangel <angel.ordonez.r@outlook.com>
2022-12-07 23:43:54 -0600

    Clase 3 Parte 4 - La Extracción.

--- curso_cero/extraccion.py
+++ curso_cero/extraccion.py
@@ -1,3 +1,3 @@
-mensaje = "Se manipula la cadena con el método de Extracción y ya"
+mensaje = "Se manipula la cadena con el método de Extracción para imprimir la subcadena de caracteres deseada de las posiciones marcadas."
 extraccion = mensaje[39:49]
 print(extraccion)
