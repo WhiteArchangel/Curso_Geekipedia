8576509164a342bab37e59e8660fa208abe2d87f
WhiteArchangel <angel.ordonez.r@outlook.com>
2022-12-08 00:02:16 -0600

    Clase 5 - Los Comentarios.

--- curso_cero/comentarios.py
+++ curso_cero/comentarios.py
@@ -3,9 +3,9 @@ print("Se ocuparán las distintas formas de generar un comentario.")
 print("La primera forma es con el numeral.")
 #Y este comentario será ignorado.
 print("Ahora se ocupará las comillas dobles sin tener un comando/variable que englobe éste.")
-"Este comentario no se imprimirá"
+"Este comentario no se imprimirá."
 print("Y por último es con comillas triples que servirá para comentarios multilínea.")
-"""Y por último
+"""Y por último,
 esto
 no será mostrado 
 en la consola."""
