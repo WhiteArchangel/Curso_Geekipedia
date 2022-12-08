c14626ac91e3155a313841f8f0199549dd0200cd
WhiteArchangel <angel.ordonez.r@outlook.com>
2022-12-07 23:56:31 -0600

    Clase 3 Parte 6 - Concatenación de texto y número.

--- curso_cero/concate_de_texto_y_numero.py
+++ curso_cero/concate_de_texto_y_numero.py
@@ -1,6 +1,6 @@
-print("Esto es un ejemplo de concatenación de texto y número")
+print("Esto es un ejemplo de concatenación de texto y número, usando la función str.")
 NumeroUno = 10
 NumeroDos = 90
 suma = NumeroUno + NumeroDos
 suma = str(suma)
-print ("El resultado de la suma es:" + suma)
+print ("El resultado de la suma es: " + suma)
