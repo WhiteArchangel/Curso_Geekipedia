70a25c8d91bbbb64f20c39fe9d2de2dffe87c698
WhiteArchangel <angel.ordonez.r@outlook.com>
2022-12-07 23:48:40 -0600

    Clase 3 Parte 5 - La Comparación.

--- curso_cero/comparacion.py
+++ curso_cero/comparacion.py
@@ -1,7 +1,7 @@
-primer_mensaje = "Python"
-segundo_mensaje = "Python"
+primer_mensaje = "Sí es Python"
+segundo_mensaje = "Sí es Python"
 print(primer_mensaje == segundo_mensaje)
 
-message_one = "Python"
-message_two = "No Python"
+message_one = "Sí es Python"
+message_two = "No es Python"
 print(message_one == message_two)
